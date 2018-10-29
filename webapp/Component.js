d02a5d36e4b9ea63777cfcb56da5cbae1fec0d19
JeremyGuan <silence-23@hotmail.com>
2018-10-29 14:52:07 +0800

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("dissona.materialmaterial.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+jQuery.sap.require("sap.m.MessageToast");
+sap.ui.generic.app.AppComponent.extend("dissona.materialmaterial.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
---
+++ webapp/ext/controller/Mybutton.controller.js
@@ -0,0 +1,5 @@
+sap.ui.controller("dissona.materialmaterial.ext.controller.Mybutton", {
+	onAction01: function(){
+		sap.m.MessageToast.show("ok!!!!!!!!");
+	}	
+});
